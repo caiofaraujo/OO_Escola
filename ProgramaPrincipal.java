e4b2a8892fbd0a1547e19b87a8991fab4fa5cf55
caiofaraujo <caiofaraujo21@hotmail.com>
2017-03-21 22:03:19 -0300

    Create ProgramaPrincipal.java

---
+++ ProgramaPrincipal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OO_Atividade;
+
+/**
+ *
+ * @author caio.faraujo
+ */
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        
+        Diretor diretor = new Diretor("Caio", 10000.0);
+        Professor professor = new Professor("Biola", 1000.0);
+        Coordenador coordenador = new Coordenador("Beiço", 5000.0);       
+        
+        
+        
+        
+    }
+}
