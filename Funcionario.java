dde3329cadc4dce5b33861a8a59de77690bca57f
caiofaraujo <caiofaraujo21@hotmail.com>
2017-03-21 22:03:41 -0300

    Create Funcionario.java

---
+++ Funcionario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OO_Atividade;
+
+/**
+ *
+ * @author caio.faraujo
+ */
+public class Funcionario {
+    public String nome;
+    
+    public String getNome() {
+        return nome;
+    }    
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
